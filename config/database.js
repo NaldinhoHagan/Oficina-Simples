3c2dda96e9c3e0f9152cce5f7e504143d6ecc0f1
NaldinhoHagan <48924297+NaldinhoHagan@users.noreply.github.com>
2026-03-23 08:18:10 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
 
+console.log("USANDO DATABASE_URL:", process.env.DATABASE_URL);
+
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL não está definida no ambiente");
+  throw new Error("DATABASE_URL não está definida");
 }
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
